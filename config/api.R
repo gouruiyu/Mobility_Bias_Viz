a038f7f83e81123d265d2d670b67aca1007eabf8
Leqi(Nancy) Wan <46666414+nancywan1004@users.noreply.github.com>
2021-07-21 01:22:24 +0800

    Delete api.R

--- config/api.R
+++
@@ -1,4 +0,0 @@
-# TODO: make sure it is not exposed before deployment without auth
-
-imgret_base <- "https://to4ho59qwg.execute-api.us-east-1.amazonaws.com/prod/"
-imgret_endpoint <- "imgretriever"
