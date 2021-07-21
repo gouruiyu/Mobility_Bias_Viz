252c37cfc4e41668434389c59335792a0eeb6684
Ruiyu Gou <reyget42@gmail.com>
2021-07-21 14:15:51 -0700

    Workaround to bypass realtive path limitation

--- stats/undercount_model.R
+++ stats/undercount_model.R
@@ -1,1 +1,9 @@
 # Bias correction model for undercounting
+# Wrap in the function purely to make the global env clean
+# i.e. dataframe loaded should not be accessible from the global env
+
+# return an OLS model to predict counted from detected
+fitModel <- function(data) {
+  return(NULL)
+}
+
